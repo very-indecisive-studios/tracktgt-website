7db642dcd6d7f8edf9a2d4c4c13013aafc6b3d03
rin <sadmeowkins@outlook.com>
2022-05-11 17:28:06 +0800

    Responsive layout for viewing game

--- app/routes/home/games/$id.tsx
+++ app/routes/home/games/$id.tsx
@@ -1,4 +1,4 @@
-﻿import { Chip, Container, Group, Image, Skeleton, Stack, Text, ThemeIcon, Title } from "@mantine/core";
+﻿import { Chip, Container, Grid, Group, Image, MediaQuery, Skeleton, Stack, Text, ThemeIcon, Title } from "@mantine/core";
 import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { backendAPIClientInstance, GetGameResult } from "backend";
@@ -15,36 +15,64 @@ export const loader: LoaderFunction = async ({params}) => {
     }
 }
 
-export default function Game() {
-    const game = useLoaderData<GetGameResult>();
+interface GameHeaderProps {
+    coverImageURL: string | undefined;
+    title: string | undefined;
+    rating: number | undefined;
+    platforms: string[] | undefined;
+    companies: string[] | undefined;
+}
+
+export function GameHeader({coverImageURL, title, rating, platforms, companies}: GameHeaderProps) {
     const [isImageLoaded, setIsImageLoaded] = useState(false);
     const imageRef = useRef<HTMLImageElement>(null);
     useEffect(() => {
         setIsImageLoaded(true);
     }, [imageRef.current?.complete])
-    
+
+    return (
+        <>
+            <Image mr={12} imageRef={imageRef} onLoad={() => setIsImageLoaded(true)} src={coverImageURL} width={200}
+                   height={300}
+                   radius={"md"} hidden={!isImageLoaded}/>
+            {!isImageLoaded && <Skeleton width={200} height={300} radius={"md"}/>}
+            <Stack>
+                <Title order={1}>{title}</Title>
+                <Title order={4}>{companies?.join(", ")}</Title>
+                <Group>
+                    <ThemeIcon color={"yellow"}>
+                        <Star size={16}/>
+                    </ThemeIcon>
+                    <Text size={"sm"}>{rating === 0 ? "No rating" : `${rating?.toFixed(0)}%`}</Text>
+                </Group>
+                <Group>
+                    {platforms?.map(platform => (
+                        <Chip checked={false} size={"sm"} key={platform}>{platform}</Chip>))}
+                </Group>
+            </Stack>
+        </>
+    );
+}
+
+export default function Game() {
+    const game = useLoaderData<GetGameResult>();
+
     return (
         <Container py={16}>
-            <Group align={"end"} noWrap>
-                <Image imageRef={imageRef} onLoad={() => setIsImageLoaded(true)} src={game.coverImageURL} width={200} height={300}
-                       radius={"md"} hidden={!isImageLoaded}/>
-                { !isImageLoaded && <Skeleton width={200} height={300} radius={"md"}/>}
-                <Stack ml={12}>
-                    <Title order={1}>{game.title}</Title>
-                    <Title order={4}>{game.companies?.join(", ")}</Title>
-                    <Group>
-                        <ThemeIcon color={"yellow"}>
-                            <Star size={16}/>
-                        </ThemeIcon>
-                        <Text size={"sm"}>{game.rating === 0 ? "No rating" : `${game.rating?.toFixed(0)}%`}</Text>
-                    </Group>
-                    <Group>
-                        {game.platforms?.map(platform => (
-                            <Chip checked={false} size={"sm"} key={platform}>{platform}</Chip>))}
-                    </Group>
+            <MediaQuery styles={{display: "none"}} largerThan={"sm"}>
+                <Stack>
+                    <GameHeader coverImageURL={game.coverImageURL} title={game.title} rating={game.rating}
+                                platforms={game.platforms} companies={game.companies}/>
                 </Stack>
-
-            </Group>
+            </MediaQuery>
+            
+            <MediaQuery styles={{display: "none"}} smallerThan={"sm"}>
+                <Group align={"end"} noWrap>
+                    <GameHeader coverImageURL={game.coverImageURL} title={game.title} rating={game.rating}
+                                platforms={game.platforms} companies={game.companies}/>
+                </Group>
+            </MediaQuery>
+            
             <Text mt={32}>{game.summary}</Text>
         </Container>
     );
