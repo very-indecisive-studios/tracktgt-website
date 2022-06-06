44d901f76b4cc04cf04716cfeb62d2734b9f0afa
Mehvin <melvin_omg@ymail.com>
2022-06-06 14:08:20 +0800

    Attempt on shows UI

---
+++ app/routes/home/shows/$id.tsx
@@ -0,0 +1,247 @@
+﻿import { Button, Container, Group, MediaQuery, Stack, Text, Title, } from "@mantine/core";
+import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData, useSubmit } from "@remix-run/react";
+import {
+    AddShowTrackingCommand,
+    backendAPIClientInstance,
+    ShowTrackingStatus,
+    ShowType,
+    GetShowResult,
+    GetShowTrackingResult,
+    RemoveShowTrackingCommand,
+    UpdateShowTrackingCommand
+} from "backend";
+import { requireUserId } from "~/utils/session.server";
+import CoverImage from "~/components/home/CoverImage";
+import { z } from "zod";
+import { badRequest } from "~/utils/response.server";
+import { Edit, Plus } from "tabler-icons-react";
+import { showTrackShowEditorModal } from "~/components/home/shows/TrackShowEditorModal";
+import { useModals } from "@mantine/modals";
+
+interface LoaderData {
+    show: GetShowResult;
+    showTracking: GetShowTrackingResult | null;
+}
+
+export const loader: LoaderFunction = async ({ params, request }) => {
+    const showId: number = parseInt(params.id ?? "0");
+
+    const getShowResponse = await backendAPIClientInstance.show_GetMovie(showId);
+    const show = getShowResponse.result;
+
+    const userId = await requireUserId(request);
+
+    const getShowTrackingResponse = await backendAPIClientInstance.show_GetShowTracking(userId, showId);
+
+    return json<LoaderData>({
+        show: show,
+        showTracking: getShowTrackingResponse.result
+    });
+}
+
+const handleDelete = async (request: Request) => {
+    const userId = await requireUserId(request);
+
+    const preProcessToNumber = (value: unknown) => (typeof value === "string" ? parseInt(value) : value);
+    let formData = Object.fromEntries(await request.formData())
+
+    // Validate form.
+    const formDataSchema = z
+        .object({
+            showRemoteId: z.preprocess(preProcessToNumber, z.number())
+        });
+
+    const parsedFormData = formDataSchema.safeParse(formData);
+
+    if (!parsedFormData.success) {
+        return badRequest(parsedFormData.error.flatten().fieldErrors);
+    }
+
+    await backendAPIClientInstance.show_RemoveShowTracking(new RemoveShowTrackingCommand({
+        showRemoteId: parsedFormData.data.showRemoteId,
+        userRemoteId: userId
+    }));
+
+    return null;
+}
+
+const parseAndValidateFormData = (formData: { [p: string]: FormDataEntryValue }) => {
+    const showStatusesLength = Object.keys(ShowTrackingStatus)
+        .filter((s) => isNaN(Number(s)))
+        .length;
+
+    const showTypesLength = Object.keys(ShowType)
+        .filter((s) => isNaN(Number(s)))
+        .length;
+
+    // Validate form.
+    const preProcessToNumber = (value: unknown) => (typeof value === "string" ? parseInt(value) : value);
+    const formDataSchema = z
+        .object({
+            showRemoteId: z.preprocess(preProcessToNumber, z.number()),
+            episodesWatched: z.preprocess(preProcessToNumber, z.number().gte(0)),
+            status: z.preprocess(preProcessToNumber, z.number().min(0).max(showStatusesLength - 1)),
+            showType: z.preprocess(preProcessToNumber, z.number().min(0).max(showTypesLength - 1))
+        });
+
+    return formDataSchema.safeParse(formData);
+}
+
+const handlePost = async (request: Request) => {
+    const userId = await requireUserId(request);
+
+    let formData = Object.fromEntries(await request.formData())
+
+    const parsedFormData = parseAndValidateFormData(formData);
+
+    if (!parsedFormData.success) {
+        return badRequest(parsedFormData.error.flatten().fieldErrors);
+    }
+
+    await backendAPIClientInstance.show_AddShowTracking(new AddShowTrackingCommand({
+        showRemoteId: parsedFormData.data.showRemoteId,
+        userRemoteId: userId,
+        episodesWatched: parsedFormData.data.episodesWatched,
+        status: parsedFormData.data.status,
+        showType: parsedFormData.data.showType
+    }));
+
+    return null;
+}
+
+const handlePut = async (request: Request) => {
+    const userId = await requireUserId(request);
+
+    let formData = Object.fromEntries(await request.formData())
+
+    const parsedFormData = parseAndValidateFormData(formData);
+
+    if (!parsedFormData.success) {
+        return badRequest(parsedFormData.error.flatten().fieldErrors);
+    }
+
+    await backendAPIClientInstance.show_UpdateShowTracking(new UpdateShowTrackingCommand({
+        showRemoteId: parsedFormData.data.showRemoteId,
+        userRemoteId: userId,
+        episodesWatched: parsedFormData.data.episodesWatched,
+        status: parsedFormData.data.status
+    }));
+
+    return null;
+}
+
+export const action: ActionFunction = async ({ request }) => {
+    if (request.method === "POST") {
+        return handlePost(request);
+    } else if (request.method === "DELETE") {
+        return handleDelete(request);
+    } else if (request.method === "PUT") {
+        return handlePut(request);
+    } else {
+        return json({ message: "Method not allowed" }, 405);
+    }
+}
+
+interface ShowHeaderProps {
+    coverImageURL: string | undefined;
+    title: string | undefined;
+    showType: ShowType | undefined;
+    hasGameTracking: boolean;
+    onAddClick: () => void;
+    onEditClick: () => void;
+}
+
+export function ShowHeader({
+                               coverImageURL,
+                               title,
+                               showType, 
+                               hasGameTracking, 
+                               onAddClick, 
+                               onEditClick
+                           }: ShowHeaderProps) {
+    // @ts-ignore
+    // @ts-ignore
+    return (
+        <>
+            <Stack mr={12}>
+                <CoverImage src={coverImageURL} width={200} height={300}/>
+
+                {!hasGameTracking &&
+                    <Button color={"indigo"}
+                            onClick={onAddClick}
+                            leftIcon={<Plus size={20}/>}>
+                        Create tracking
+                    </Button>}
+
+                {hasGameTracking &&
+                    <Button color={"orange"}
+                            onClick={onEditClick}
+                            leftIcon={<Edit size={20}/>}>
+                        Edit tracking
+                    </Button>}
+            </Stack>
+
+            <Stack spacing={"xs"}>
+                <Title order={1}>
+                    {title}
+                </Title>
+
+                <Title order={4} sx={(theme) => ({
+                    color: theme.colors.gray[6],
+                })}>
+                    {showType}
+                </Title>
+            </Stack>
+        </>
+    );
+}
+
+export default function Show() {
+    const loaderData = useLoaderData<LoaderData>();
+    const modals = useModals();
+    const submit = useSubmit();
+
+    const showHeader = <ShowHeader coverImageURL={loaderData.show.coverImageURL}
+                                   title={loaderData.show.title}
+                                   showType={loaderData.show.showType}
+                                   hasShowTracking={!!loaderData.showTracking}
+                                   onAddClick={() => showTrackShowEditorModal(
+                                       modals,
+                                       loaderData.show,
+                                       null,
+                                       (formData) => submit(formData, { method: "post", replace: true }),
+                                       () => { },
+                                       () => { }
+                                   )}
+                                   onEditClick={() => showTrackShowEditorModal(
+                                       modals,
+                                       loaderData.show,
+                                       loaderData.showTracking,
+                                       () => { },
+                                       (formData) => submit(formData, { method: "put", replace: true }),
+                                       (formData) => submit(formData, { method: "delete", replace: true })
+                                   )}/>
+
+    return (
+        <Container py={16}>
+            <MediaQuery styles={{ display: "none" }} largerThan={"sm"}>
+                <Stack>
+                    {showHeader}
+                </Stack>
+            </MediaQuery>
+
+            <MediaQuery styles={{ display: "none" }} smallerThan={"sm"}>
+                <Group align={"end"} noWrap>
+                    {showHeader}
+                </Group>
+            </MediaQuery>
+
+            <Stack mt={48}>
+                <Title order={2}>Summary</Title>
+                <Text
+                    sx={(theme) => ({ color: theme.colors.gray[6] })}>{loaderData.show.summary?.replace(/<\/?[^>]+(>|$)/g, "")}</Text>
+            </Stack>
+        </Container>
+    );
+}
---
+++ app/routes/home/shows/search.tsx
@@ -0,0 +1,70 @@
+﻿import {Card, Container, Group, Stack, Text, Title} from "@mantine/core";
+import {json, LoaderFunction} from "@remix-run/node";
+import {backendAPIClientInstance, SearchShowsItemResult, ShowType} from "backend";
+import {Link, useLoaderData, useSearchParams} from "@remix-run/react";
+import CoverImage from "~/components/home/CoverImage";
+
+interface LoaderData {
+    items: SearchShowsItemResult[]
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+    const url = new URL(request.url);
+    const title = url.searchParams.get("title");
+
+    const backendResult = await backendAPIClientInstance.show_SearchShows(title);
+
+    return json<LoaderData>({
+        items: backendResult.result.items ?? []
+    });
+}
+
+interface SearchResultItemProps {
+    id: number;
+    title: string;
+    coverImageURL: string | undefined;
+    showType: ShowType;
+}
+
+function SearchResultItem({ id, title, coverImageURL, showType }: SearchResultItemProps) {
+    return (
+        <div style={{ width: "100%", margin: 'auto' }}>
+            <Link to={`/home/shows/${id}`} style={{ textDecoration: "none" }}>
+                <Card shadow="xs" p="lg">
+                    <Group align={"end"}>
+                        <CoverImage src={coverImageURL} width={100} height={150}/>
+
+                        <Stack ml={8}>
+                            <Title order={3}>{title}</Title>
+                            <Text>
+                                {showType.toString()}
+                            </Text>
+                        </Stack>
+                    </Group>
+                </Card>
+            </Link>
+        </div>
+    );
+}
+
+export default function Search() {
+    const loaderData = useLoaderData<LoaderData>();
+    const [searchParams, _] = useSearchParams();
+    const title = searchParams.get("title");
+
+    return (
+        <Container py={16}>
+            <Title mb={32} order={2}>Search results for "{title}"</Title>
+            <Stack>
+                {loaderData.items.map(s => (
+                    <SearchResultItem key={s.remoteId ?? ""}
+                                      id={s.remoteId ?? 0}
+                                      title={s.title ?? ""}
+                                      coverImageURL={s.coverImageURL}
+                                      showType={s.showType ?? ShowType.Movie}
+                    />
+                ))}
+            </Stack>
+        </Container>
+    );
+}
