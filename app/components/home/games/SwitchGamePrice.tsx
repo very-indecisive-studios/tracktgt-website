8509c248d5b4793c39ef6ccd70a4ee9440af7212
shazxrin <70547991+shazxrin@users.noreply.github.com>
2022-06-24 13:52:09 +0800

    [FEATURE] Pricing for games UI (Nintendo Switch) (#33)
    * Pricing UI in wishlist and game page
    
    * User preferences setup and UI
    
    * Load game price based on user's preferred region
    
    * Hide table if no price store available

---
+++ app/components/home/games/SwitchGamePrice.tsx
@@ -0,0 +1,31 @@
+import { useSwitchGamePrice } from "~/routes/home/games/price/switch/$id";
+import { Group, Loader, Text } from "@mantine/core";
+import { Minus, TrendingDown } from "tabler-icons-react";
+import React from "react";
+
+interface SwitchGamePriceProps {
+    gameRemoteId: number;
+}
+
+export default function SwitchGamePrice({ gameRemoteId }: SwitchGamePriceProps) {
+    const { price, isLoading } = useSwitchGamePrice(gameRemoteId);
+
+    return (
+        <>
+            {isLoading ?
+                <Loader size="xs" variant="bars" /> :
+                (!price) ?
+                    <Text>N/A</Text>:
+                    <Group spacing={"xs"}>
+                        { price.isOnSale ? <TrendingDown color={"green"} size={22} /> : <Minus size={22} />}
+                        <Text variant={"link"} component={"a"} href={price.url} sx={(theme) => ({
+                            color: price?.isOnSale ? theme.colors.green[4] : theme.colors.dark[1],
+                            textDecoration: "none"
+                        })}>
+                            {price.currency} {price.price?.toFixed(2)}
+                        </Text>
+                    </Group>
+            }
+        </>
+    )
+}
